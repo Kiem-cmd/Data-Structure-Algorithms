df79bf8d0c06a2e62a935da6a9d7099f5c6e4413
Kiem <85799436+Kiem-cmd@users.noreply.github.com>
2023-02-16 14:36:46 +0700

    Create Bieu_dien_Graphs.cpp

---
+++ Graphs/Bieu_dien_Graphs.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main(){
+	// n : so nut
+	//  m : so canh 
+	int n,m ;  
+	cin >>n >> m;
+	// su dung ma tran ke: do phuc tap O(n)
+	int adj[n+1][n+1];
+	for(int i =0;i<n;i++){
+		int u,v;
+		cin>> u >>v ;
+		adj[u][v] = 1;
+		adj[v][u] = 1;
+	}
+	
+	// su dung danh sach ke : do phuc tap O(2xEdge)
+	vector<int> adj[n+1];
+	for (int i =0; i<m;i++){
+		int u,v;
+		cin >>u>>v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	/// voi do thi co huong chi luu 1 chieu voi ca hai cach 
+	
+	/// voi do thi co trong so 
+	/// - voi ma tran ke thi thay 1 bang trong so 
+	/// - voi danh sach ke thì tao vector<pair<int,int>> adj[n+1]
+	return 0;
+}
