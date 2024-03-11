407c0fa1738c9c4c963ffdd56cf93cbfc9d4190a
Kiem <phamxuankiem27122001@gmail.com>
2024-03-11 10:40:32 +0700

    Stack Day 3

--- Stack/hackerrank/Lagest_retangle.cpp
+++ Stack/hackerrank/Lagest_retangle.cpp
@@ -1,7 +1,105 @@
-#include<bits/stdc++.h> 
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'largestRectangle' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY h as parameter.
+ */
+
+long largestRectangle(vector<int> heights) {
+        heights.push_back(0);
+        heights.insert(heights.begin(),0);
+        stack<pair<int,int>> s; 
+        int result = 0;
+        for(int i = 0; i< heights.size(); i++){
+            if(s.empty() || s.top().second <= heights[i]){
+                s.push(pair<int,int>(i,heights[i]));
+            }
+            else{
+                while(s.top().second > heights[i]){
+                    pair<int,int> temp = s.top();
+                    s.pop();
+                    result = result > ((i - s.top().first - 1) * temp.second) ? result : ((i -1 - s.top().first) * temp.second);            
+                }
+                s.push(pair<int,int>(i,heights[i]));
+            }
+        }
+        return result;
+}
+//// 2 4 5 3 6 
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split(rtrim(h_temp_temp));
+
+    vector<int> h(n);
+
+    for (int i = 0; i < n; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    long result = largestRectangle(h);
+
+    fout << result << "\n";
+
+    fout.close();
+
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
